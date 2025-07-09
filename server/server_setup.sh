2fdbe062c8500c7854ca50b4e73e331850d71bbc
graxel <grazel98@gmail.com>
2025-07-09 09:45:10 -0400

    organized server setup files

--- server/server_setup.sh
+++ server/server_setup.sh
@@ -8,9 +8,9 @@ sudo systemctl start mosquitto
 
 # create systemd service files
 USER_NAME=$(whoami)
-sed "s/__USERNAME__/${USER_NAME}/g" mqtt_postgres_bridge.service | sudo tee /etc/systemd/system/mqtt_postgres_bridge.service > /dev/null
-sed "s/__USERNAME__/${USER_NAME}/g" summarizer.service | sudo tee /etc/systemd/system/summarizer.service > /dev/null
-sudo cp summarizer.timer /etc/systemd/system/
+sed "s/__USERNAME__/${USER_NAME}/g" service_files/mqtt_postgres_bridge.service | sudo tee /etc/systemd/system/mqtt_postgres_bridge.service > /dev/null
+sed "s/__USERNAME__/${USER_NAME}/g" service_files/summarizer.service | sudo tee /etc/systemd/system/summarizer.service > /dev/null
+sudo cp service_files/summarizer.timer /etc/systemd/system/
 
 # enable services
 sudo systemctl daemon-reload
--- server/system_setup.sh
+++
@@ -1,4 +0,0 @@
-sudo apt update
-sudo apt install mosquitto mosquitto-clients -y
-sudo systemctl enable mosquitto
-sudo systemctl start mosquitto
