243e1c578a92dca3b3df8d969342a9472a0ea60f
graxel <grazel98@gmail.com>
2025-07-18 17:15:39 -0400

    new data format from server

--- server/data_provider.py
+++ server/data_provider.py
@@ -1,12 +1,39 @@
 import random
 import asyncio
+from datetime import datetime
+
 
 async def get_data():
     # Simulate some async data fetching (e.g., querying a database)
     await asyncio.sleep(0)  # just an example async placeholder
     
-    value = random.randint(1, 100)
-    return {"value": value}
+    value = random.randint(20, 27)
+    data = {
+        "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "sensor_data": {
+            "office_temp": value,
+            "office_hum": 45,
+            "kitchen_temp": value + 2,
+            "kitchen_hum": 50,
+            "closet_temp": 25,
+            "closet_hum": 48,
+            "bedroom_temp": 23,
+            "bedroom_hum": 51
+        },
+        "home_data": {
+            "ac_on": 0,
+            "windows_open": 0,
+            "door_open": 0
+        },
+        "weather_data": {
+            "sun_direction": 114,
+            "sun_altitude": 20,
+            "cloud_cover": 30,
+            "temperature": 87,
+            "humidity": 67
+        }
+    }
+    return {"data": data}
 
 
 if __name__ == "__main__":
--- server/server_setup.sh
+++ server/server_setup.sh
@@ -9,6 +9,7 @@ sudo systemctl start mosquitto
 # create systemd service files
 USER_NAME=$(whoami)
 sed "s/__USERNAME__/${USER_NAME}/g" service_files/mqtt_postgres_bridge.service | sudo tee /etc/systemd/system/mqtt_postgres_bridge.service > /dev/null
+sed "s/__USERNAME__/${USER_NAME}/g" service_files/websocket_server.service | sudo tee /etc/systemd/system/websocket_server.service > /dev/null
 sed "s/__USERNAME__/${USER_NAME}/g" service_files/summarizer.service | sudo tee /etc/systemd/system/summarizer.service > /dev/null
 sudo cp service_files/summarizer.timer /etc/systemd/system/
 
